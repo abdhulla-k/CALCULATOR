daa0f3e5ce904c3fd14f58508d6745628dad75da
abdhu-omy <abdhullak149@gmail.com>
2021-07-25 00:31:11 +0530

    currections in input method

--- Basic_Calculator/Basic_Calculator.py
+++ Basic_Calculator/Basic_Calculator.py
@@ -1,21 +1,5 @@
 from tkinter import *
 
-window = Tk()   # created a basic window
-window.geometry( "500x500" )   # it is to set the measure of the basic window
-window.title( "Basic Calculator" ) # created title
-window.configure( background= "grey" )  # to determine the color
-
-
-input_text = StringVar   # this is to receive input
-input_frame = Frame( window, background= "#4374ab", width= 500, height= 50 ) # frame created for input_frame
-input_frame.pack( side= TOP )
-
-
-# now let's create an input field
-input_field = Entry( input_frame, width= 500, font= ('arial', 18, 'bold'), textvariable= input_text, justify= RIGHT,
- bd= 0, bg= "black" )
-input_field.pack( ipady= 10 )    # ipady is internal padding to increase the height of input field
-
 
 # Now let's create methods
 
@@ -26,6 +10,7 @@ def clear():       # it is the method of 'clear' button.
     expression = ""
     input_text.set( "" )
 
+
 def equal():      # it is for '=' button
     global expression
     expression = ""
@@ -34,12 +19,28 @@ def equal():      # it is for '=' button
     #  the result as an integer.
     input_text.set( result )
 
+
 def scan( self ):    # int is the selection metnod.this function continuously updates the input whenever enters a number
     global expression
     expression = expression + str( self )
     input_text.set( expression )
 
 
+window = Tk()   # created a basic window
+window.geometry( "355x355" )   # it is to set the measure of the basic window
+window.title( "Basic Calculator" ) # created title
+window.configure( background= "grey" )  # to determine the color
+
+
+input_text = StringVar   # this is to receive input
+# now let's create an input field
+input_field = Entry( window, width= 16, font= ('arial', 18, 'bold'), textvariable= input_text, justify= RIGHT,
+ bd= 0, bg= "black" )
+input_field.grid( ipady= 10, ipadx= 70, columnspan= 4 )    # ipady is internal padding to increase the height of input field
+
+
+
+# create buttons
 
 
 
