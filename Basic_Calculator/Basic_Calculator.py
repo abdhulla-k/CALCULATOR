38ea8ca0313d73e1a9b043684b16394ab82ef7f6
abdhu-omy <abdhullak149@gmail.com>
2021-07-24 18:29:43 +0530

    created input field

--- Basic_Calculator/Basic_Calculator.py
+++ Basic_Calculator/Basic_Calculator.py
@@ -5,9 +5,15 @@ window.geometry( "500x500" )   # it is to set the measure of the basic window
 window.title( "Basic Calculator" ) # created title
 window.configure( background= "grey" )  # to determine the color
 
-input_frame = StringVar   # this is to receive input
+input_text = StringVar   # this is to receive input
 input_frame = Frame( window, background= "#4374ab", width= 500, height= 50 ) # frame created for input_frame
-input_frame.pack()  
+input_frame.pack( side= TOP )
+
+# now let's create an input field
+input_field = Entry( input_frame, width= 500, font= ('arial', 18, 'bold'), textvariable= input_text, justify= RIGHT,
+ bd= 0, bg= "black" )
+input_field.pack( ipady= 10 )    # ipady is internal padding to increase the height of input field
+
 
 
 
